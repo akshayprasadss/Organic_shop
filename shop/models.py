43a37f1c96e120bfa0e34f8077a4a98ce8553341
akshayprasadss <akshayprasad014@gmail.com>
2025-07-06 22:28:30 +0530

    models.py

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 
 CATEGORIES = [
@@ -38,44 +37,3 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
-=======
-from django.db import models
-
-CATEGORIES = [
-    ('Fruits', 'Fruits'),
-    ('Vegetables', 'Vegetables'),
-    ('Bread', 'Bread'),
-    ('Meat', 'Meat'),
-]
-
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.FloatField()
-    description = models.TextField()
-    image = models.ImageField(upload_to='products/')
-    category = models.CharField(max_length=100, choices=CATEGORIES)  # ← Changed here
-
-    def __str__(self):
-        return self.name
-
-
-class Review(models.Model):
-    name = models.CharField(max_length=100)
-    profession = models.CharField(max_length=100, blank=True, null=True)
-    message = models.TextField()
-    photo = models.ImageField(upload_to='reviews/', blank=True, null=True)
-    rating = models.IntegerField(default=5)
-
-    def __str__(self):
-        return self.name
-
-
-class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    message = models.TextField()
-    submitted_at = models.DateTimeField(auto_now_add=True)  
-
-    def __str__(self):
-        return self.name
->>>>>>> 13155b5 (Add choices to category field in Product model)
