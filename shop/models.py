e27121f47d6fe39ec6ea8b7a57b0300e4d3e0269
Akshay Prasad <akshayprasadss@gmail.com>
2025-07-06 17:00:46 +0000

    Remove duplicate code in models.py

--- shop/models.py
+++ shop/models.py
@@ -33,7 +33,50 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
-    submitted_at = models.DateTimeField(auto_now_add=True)  
+    submitted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+<<<<<<< HEAD
+=======
+
+from django.db import models
+
+CATEGORIES = [
+    ('Fruits', 'Fruits'),
+    ('Vegetables', 'Vegetables'),
+    ('Bread', 'Bread'),
+    ('Meat', 'Meat'),
+]
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    category = models.CharField(max_length=100, choices=CATEGORIES)  # ← Changed here
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    name = models.CharField(max_length=100)
+    profession = models.CharField(max_length=100, blank=True, null=True)
+    message = models.TextField()
+    photo = models.ImageField(upload_to='reviews/', blank=True, null=True)
+    rating = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+>>>>>>> b8a9a33 (Remove duplicate code in models.py)
